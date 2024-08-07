57e56a0fdd561b3a70314c9399aaf674ab2aea55
[mahtot] <[mahtotgher13@gmail.com]>
2024-08-07 16:47:40 +0300

    Modified the footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import logo from "../assets/imgs/logo.png";
 import { FaXTwitter, FaFacebookF } from "react-icons/fa6";
 import { FaInstagram, FaLinkedinIn, FaGithub } from "react-icons/fa";
-import { NavLink,  useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 function Footer() {
     const menuItems = [
@@ -9,54 +9,37 @@ function Footer() {
         { name: "Tour Packages", path: "/tour-packages" },
         { name: "Contact", path: "/contact" },
         { name: "Account", path: "/account" },
-      ];
+    ];
 
-      // To check if tour-packages is active 
-  function isTourPackagesActive(pathname) {
-    return pathname.startsWith('/tour-packages') || pathname === '/view-details' || pathname === '/book-package';
-  }
+    // To check if tour-packages is active 
+    function isTourPackagesActive(pathname) {
+        return pathname.startsWith('/tour-packages') || pathname === '/view-details' || pathname === '/book-package';
+    }
 
-
-  return (
-    <div className="flex flex-col w-full font-montserrat mt-[100px] bg-slate-50 gap-2">
-        <div className="flex  flex-col 627:flex-row justify-center items-center ">
-        <div className="flex  flex-col 627:flex-row  justify-center items-center gap-7 p-10">
-            <img src={logo}
-                alt="Company logo"
-                className="w-[70vw] 627:w-[200px] 627:h-[30px]"/>
-            <div className=" hidden 627:flex-col 627:flex w-[40vw] justify-center items-center">
-                {menuItems.map((item, index) => (
-                    <NavLink
-                    to={item.path}
-                    key={index}
-                    onClick={()=>setIsMenuOpen(false)}
-                    className={({ isActive }) =>
-                        index==2?
-                        isTourPackagesActive(location.pathname) ? 'text-[#FC6C22]' : 'text-black'
-                        :  isActive ? 'text-[#FC6C22]' : 'text-black'
-
-                    }
-                    >
-                    <div className="hover:text-[#FC6C22] hover:transition-all p-2 text-left w-[200px] 1097:w-auto">
+    return (
+        <footer className="flex flex-col w-full font-montserrat mt-[100px] bg-[#264F0B] p-6 text-white text-[13px] 531:text-[16px]">
+            <div className="flex flex-col items-center gap-4">
+                <img src={logo} alt="Logo" className="h-12" />
+                <div className="flex gap-5 text-2xl">
+                    <a href="#" title="Twitter" className="hover:text-gray-300"><FaXTwitter /></a>
+                    <a href="#" title="Instagram" className="hover:text-gray-300"><FaInstagram /></a>
+                    <a href="#" title="Facebook" className="hover:text-gray-300"><FaFacebookF /></a>
+                    <a href="https://www.linkedin.com/in/mahtot-gher-57860a274/" title="LinkedIn" className="hover:text-gray-300"><FaLinkedinIn /></a>
+                    <a href="https://github.com/Mahtot" title="GitHub" className="hover:text-gray-300"><FaGithub /></a>
+                </div>
+            </div>
+            <nav className="flex justify-center mt-4">
+                {menuItems.map(item => (
+                    <NavLink key={item.name} to={item.path} className="mx-3 hover:text-gray-300">
                         {item.name}
-                    </div>
                     </NavLink>
                 ))}
+            </nav>
+            <div className="text-center mt-6">
+                <p>© 2024 All Rights Reserved.</p>
             </div>
-        </div>
-        <div className="flex 627:flex-col justify-center items-center gap-5">
-            <a href="#" title="Twitter"><FaXTwitter /></a>
-                <a href="#" title="Instagram"><FaInstagram /></a>
-                <a href="#" title="Facebook"><FaFacebookF /></a>
-                <a href="https://www.linkedin.com/in/mahtot-gher-57860a274/" title="Linkedin"><FaLinkedinIn /></a>
-                <a href="https://github.com/Mahtot" title="Github"><FaGithub /></a>
-            
-        </div>
-        </div>
-        <div className="p-10">
-            <p>© 2024 All Rights Reserved.</p>
-        </div>
-    </div>
-  )
+        </footer>
+    );
 }
-export default Footer
+
+export default Footer;
